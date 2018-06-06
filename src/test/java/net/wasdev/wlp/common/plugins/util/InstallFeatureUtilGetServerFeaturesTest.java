96ec921cd233bf1e16e6f63b73eae4919f563c70
Eric Lau <ericglau@ca.ibm.com>
2018-06-06 17:12:47 -0400

    Update unit tests

--- src/test/java/net/wasdev/wlp/common/plugins/util/InstallFeatureUtilGetServerFeaturesTest.java
+++ src/test/java/net/wasdev/wlp/common/plugins/util/InstallFeatureUtilGetServerFeaturesTest.java
@@ -55,7 +55,7 @@ public class InstallFeatureUtilGetServerFeaturesTest {
         copy("server.xml");
 
         Set<String> expected = new HashSet<String>();
-        expected.add("jsp-2.3");
+        expected.add("orig");
 
         verifyServerFeatures(expected);
     }
@@ -71,12 +71,10 @@ public class InstallFeatureUtilGetServerFeaturesTest {
         copy("extraFeatures.xml");
 
         Set<String> expected = new HashSet<String>();
-        expected.add("jsp-2.3");
+        expected.add("orig");
 
         verifyServerFeatures(expected);
     }
-
-
 
     /**
      * Tests server.xml with MERGE function
@@ -89,8 +87,8 @@ public class InstallFeatureUtilGetServerFeaturesTest {
         copy("extraFeatures.xml");
 
         Set<String> expected = new HashSet<String>();
-        expected.add("jsp-2.3");
-        expected.add("adminCenter-1.0");
+        expected.add("orig");
+        expected.add("extra");
 
         verifyServerFeatures(expected);
     }
@@ -106,24 +104,24 @@ public class InstallFeatureUtilGetServerFeaturesTest {
         copy("extraFeatures.xml");
 
         Set<String> expected = new HashSet<String>();
-        expected.add("adminCenter-1.0");
+        expected.add("extra");
 
         verifyServerFeatures(expected);
     }
 
     /**
-     * Tests server.xml with multiple REPLACE functions
+     * Tests server.xml with nested REPLACE functions
      * 
      * @throws Exception
      */
     @Test
-    public void testMultipleReplaceXML() throws Exception{
-        copyAsName("multipleReplace.xml", "server.xml");
-        copy("multipleReplace2.xml");
-        copy("multipleReplace3.xml");
+    public void testNestedReplaceXML() throws Exception{
+        copyAsName("server_nested_replace.xml", "server.xml");
+        copy("nestedReplace2.xml");
+        copy("nestedReplace3.xml");
 
     	Set<String> expected = new HashSet<String>();
-    	expected.add("adminCenter-1.0");
+    	expected.add("nested3");
 
         verifyServerFeatures(expected);
     }
@@ -134,13 +132,13 @@ public class InstallFeatureUtilGetServerFeaturesTest {
      * @throws Exception
      */
     @Test
-    public void testConfigOverride() throws Exception{
+    public void testConfigOverrides() throws Exception{
         copy("server.xml");
         copyAsName("config_override.xml", "configDropins/overrides/config_override.xml");
 
         Set<String> expected = new HashSet<String>();
-        expected.add("jsp-2.3");
-        expected.add("adminCenter-1.0");
+        expected.add("orig");
+        expected.add("override");
 
         verifyServerFeatures(expected);
     }
@@ -151,13 +149,13 @@ public class InstallFeatureUtilGetServerFeaturesTest {
      * @throws Exception
      */
     @Test
-    public void testConfigDefault() throws Exception{
+    public void testConfigDefaults() throws Exception{
         copy("server.xml");
         copyAsName("config_default.xml", "configDropins/defaults/config_default.xml");
 
         Set<String> expected = new HashSet<String>();
-        expected.add("jsp-2.3");
-        expected.add("batch-1.0");
+        expected.add("orig");
+        expected.add("default");
 
         verifyServerFeatures(expected);
     }
@@ -168,17 +166,165 @@ public class InstallFeatureUtilGetServerFeaturesTest {
      * @throws Exception
      */
     @Test
-    public void testOverrideAndDefault() throws Exception{
+    public void testOverridesAndDefaults() throws Exception{
         copy("server.xml");
         copyAsName("config_override.xml", "configDropins/overrides/config_override.xml");
         copyAsName("config_default.xml", "configDropins/defaults/config_default.xml");
 
         Set<String> expected = new HashSet<String>();
-        expected.add("jsp-2.3");
-        expected.add("batch-1.0");
-        expected.add("adminCenter-1.0");
+        expected.add("orig");
+        expected.add("override");
+        expected.add("default");
+
+        verifyServerFeatures(expected);
+    }
+    
+    /**
+     * Tests server.xml with multiple MERGE functions
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testMultipleMerge() throws Exception{
+        copyAsName("server_multiple_merge.xml", "server.xml");
+        copy("extraFeatures2.xml");
+        copy("extraFeatures3.xml");
+
+        Set<String> expected = new HashSet<String>();
+        expected.add("orig");
+        expected.add("extra2");
+        expected.add("extra3");
+
+        verifyServerFeatures(expected);
+    }
+    
+    /**
+     * Tests server.xml with multiple REPLACE functions
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testMultipleReplace() throws Exception{
+        copyAsName("server_multiple_replace.xml", "server.xml");
+        copy("extraFeatures2.xml");
+        copy("extraFeatures3.xml");
+
+        // only the last replace should be kept
+        Set<String> expected = new HashSet<String>();
+        expected.add("extra3");
+
+        verifyServerFeatures(expected);
+    }
+    
+    /**
+     * Tests server.xml with REPLACE function that has no featureManager section
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testNoListReplace() throws Exception{
+        copyAsName("server_no_list_replace.xml", "server.xml");
+        copy("noList.xml");
+
+        // parent is kept, since child list does not exist
+        Set<String> expected = new HashSet<String>();
+        expected.add("orig");
+
+        verifyServerFeatures(expected);
+    }
+    
+    /**
+     * Tests server.xml with REPLACE function that has empty featureManager section
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testEmptyListReplace() throws Exception{
+        copyAsName("server_empty_list_replace.xml", "server.xml");
+        copy("emptyList.xml");
+
+        // parent is kept, since child list is empty
+        Set<String> expected = new HashSet<String>();
+        expected.add("orig");
 
         verifyServerFeatures(expected);
     }
 
+    /**
+     * Tests server.xml with IGNORE function that has no featureManager section
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testParentNoListIgnore() throws Exception{
+        copyAsName("server_parent_no_list_ignore.xml", "server.xml");
+        copy("extraFeatures.xml");
+
+        // do not ignore the child list since parent has no featureManager section
+        Set<String> expected = new HashSet<String>();
+        expected.add("extra");
+
+        verifyServerFeatures(expected);
+    }
+    
+    /**
+     * Tests server.xml with IGNORE function that has empty featureManager section
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testParentEmptyListIgnore() throws Exception{
+        copyAsName("server_parent_empty_list_ignore.xml", "server.xml");
+        copy("extraFeatures.xml");
+
+        // ignore the child list since parent has a featureManager section which counts as a conflict
+        Set<String> expected = new HashSet<String>();
+
+        verifyServerFeatures(expected);
+    }
+    
+    /**
+     * Tests server.xml with a combination of everything, including a replace that is empty
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testCombinationEmptyReplace() throws Exception{
+        copyAsName("server_combination_empty_replace.xml", "server.xml");
+        copyAsName("config_override.xml", "configDropins/overrides/config_override.xml");
+        copyAsName("config_default.xml", "configDropins/defaults/config_default.xml");
+        copy("extraFeatures.xml");
+
+        // Keep everything except the ignore (obviously).
+        // Do not replace existing contents since replace file is empty.
+        Set<String> expected = new HashSet<String>();
+        expected.add("orig");
+        expected.add("override");
+        expected.add("default");
+        expected.add("extra");
+        expected.add("extra3");
+        
+        verifyServerFeatures(expected);
+    }
+    
+    /**
+     * Tests server.xml with a combination of everything, including a replace that is empty
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testCombinationWithReplace() throws Exception{
+        copyAsName("server_combination_replace.xml", "server.xml");
+        copyAsName("config_override.xml", "configDropins/overrides/config_override.xml");
+        copyAsName("config_default.xml", "configDropins/defaults/config_default.xml");
+        copy("extraFeatures.xml");
+
+        // Only keep the configDropins and the replace
+        Set<String> expected = new HashSet<String>();
+        expected.add("override");
+        expected.add("default");
+        expected.add("extra3");
+        
+        verifyServerFeatures(expected);
+    }
 }
